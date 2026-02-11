bca4e2a2e5fab3bb971dc7319142ea7beb3eb5d1
byungil712 <jam1598@naver.com>
2026-02-11 14:27:37 +0900

    2026-02-11-3

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import App from "./WeatherApp/App";
 import store from "./WeatherApp/Script/store";
@@ -8,9 +8,9 @@ import store from "./WeatherApp/Script/store";
 createRoot(document.getElementById("root")).render(
    <StrictMode>
       <Provider store={store}>
-         <BrowserRouter>
+         <HashRouter>
             <App />
-         </BrowserRouter>
+         </HashRouter>
       </Provider>
    </StrictMode>,
 );
